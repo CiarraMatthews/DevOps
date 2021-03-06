2ab55dcf3ba9e739a8b5fa2b5d8927223e4f2b52
CiarraMatthews <cdmatthews@ucdavis.edu>
2021-03-06 12:39:37 -0800

    What happened?

---
+++ python.py
@@ -0,0 +1,24 @@
+lst = [1,2,3,4,5]
+
+#for variable in lst:
+#	print(variable**10, " ", end="")
+
+#for variable in lst:
+#	print(variable**10, " ", end="")
+
+lst[0:4:2]
+
+for x in range (0,11,2):
+	print(x)
+
+#print("10 / 2 =", 10/2)
+#print("10 % 2 =", 10%2)
+
+def function():
+	num = input("Type a number between 10 and 20: ")
+	if int(num) > 10 and int(num) < 20:
+		print("Yaaas", num ,"is between 10 and 20")
+	else:
+		print("boo!")
+
+function()
